5f0b1f98799fe0ef90c408d11da3ea43b6e877a9
kyoya <kyoya.p@gmail.com>
2021-04-06 06:48:59 +0900

    n

--- fsAg_NodeKt/src/main/kotlin/stressDevice.kt
+++ fsAg_NodeKt/src/main/kotlin/stressDevice.kt
@@ -1,13 +1,9 @@
 import dev.gitlive.firebase.Firebase
 import dev.gitlive.firebase.FirebaseApp
-import dev.gitlive.firebase.firestore.DocumentSnapshot
-import dev.gitlive.firebase.firestore.firestore
-import dev.gitlive.firebase.firestore.where
+import dev.gitlive.firebase.firestore.*
 import gdvm.schema.GenericDevice
 import gdvm.schema.Schedule
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.*
 import kotlinx.datetime.Clock
 import kotlinx.serialization.Serializable
@@ -44,6 +40,8 @@ suspend fun runStressDevice(fbApp: FirebaseApp, dev: GenericDevice) {
     println("start Device: ${dev}")
     val db = Firebase.firestore(fbApp)
     val devDocRef = db.collection("device").document(dev.id)
+
+    val cList = mutableMapOf<String, Int>()
     devDocRef.collection("query")
         .where("devId", equalTo = dev.id)
         .where("result", equalTo = null)
@@ -51,30 +49,55 @@ suspend fun runStressDevice(fbApp: FirebaseApp, dev: GenericDevice) {
             channelFlow { it.documents.forEach { offer(it.data<DevStressQuery>()) } }
         }.buffer(10).collect { query ->
             println("Start Query: ${query.id}")
-            repeat(query.schedule.limit) {
-                inline fun <reified T> DocumentSnapshot.dataOrDefault(d: T) = if (exists) data() else d
-                //db.runTransaction {
-                val cDocRef = devDocRef.collection("counter").document("logs_${rand.nextInt(10)}")
-                val c = cDocRef.get().dataOrDefault(Count(count = 0, devId = dev.id)).count
-                println("Count=$c") //TODO
-                cDocRef.set(Count.serializer(), Count(devId = dev.id, count = c + 1))
-                devDocRef.collection("logs").add(
-                    DevLog.serializer(), DevLog(
-                        devId = dev.id,
-                        cluster = dev.cluster,
-                        time = Clock.System.now().toEpochMilliseconds().toInt(),
-                    )
-                )
-                //}
+            val st = Clock.System.now().toEpochMilliseconds()
+            (0 until query.schedule.limit).map {
+                val j = GlobalScope.launch {
+                    val t1 = Clock.System.now().toEpochMilliseconds() - st
+                    db.runTransaction {
+                        val t2 = Clock.System.now().toEpochMilliseconds() - st
+                        val cName = "logs_${rand.nextInt(100)}"
+                        val cDocRef = devDocRef.collection("counter").document(cName)
+                        val c = cDocRef.get().dataOrDefault(Count(count = 0, devId = dev.id)).count + 1
+                        cDocRef.set(Count.serializer(), Count(devId = dev.id, count = c))
+                        devDocRef.collection("logs").add(
+                            DevLog.serializer(), DevLog(
+                                devId = dev.id,
+                                cluster = dev.cluster,
+                                time = Clock.System.now().toEpochMilliseconds().toInt(),
+                            )
+                        )
+                        cList[cName] = c
+                        val sum = cList.map { it.value }.sum()
+                        val t3 = Clock.System.now().toEpochMilliseconds() - st
+                        println("$cName=${cList[cName]} sum=$sum : t1:$t1 t2-t1:${t2 - t1} t3-t1:${t3 - t1}") //TODO
+                    }
+                }
                 delay(query.schedule.interval)
-            }
-            devDocRef.collection("counter").snapshots.collect {
-                val sum = it.documents.map { it.data<Count>().count }.sum()
-                println("Sum: $sum")
-            }
-
-            println("End Query: ${query.id}")
+                j
+            }.toList().forEach { it.join() }
+            val sum = devDocRef.collection("counter").get().documents.map { it.data<Count>().count }.sum()
+            println("Sum: $sum")
+            println("End Query: ${(Clock.System.now().toEpochMilliseconds() - st) / 1000.0} :${query.id}")
         }
 }
+
+inline fun <reified T> DocumentSnapshot.dataOrDefault(d: T) = if (exists) data() else d
+
+suspend fun logging(db: FirebaseFirestore, devDocRef: DocumentReference, dev: GenericDevice) {
+    db.runTransaction {
+        val cName = "logs_${rand.nextInt(10)}"
+        val cDocRef = devDocRef.collection("counter").document(cName)
+        val c = cDocRef.get().dataOrDefault(Count(count = 0, devId = dev.id)).count + 1
+        cDocRef.set(Count.serializer(), Count(devId = dev.id, count = c))
+        devDocRef.collection("logs").add(
+            DevLog.serializer(), DevLog(
+                devId = dev.id,
+                cluster = dev.cluster,
+                time = Clock.System.now().toEpochMilliseconds().toInt(),
+            )
+        )
+    }
+}
+
 
 
