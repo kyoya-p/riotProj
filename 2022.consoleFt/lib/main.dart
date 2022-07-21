63b8edafb5aa069723da54a34cd84ad1d61e2ceb
kyoya.p <kyoya.p@gmail.com>
2022-07-21 09:04:27 +0900

    n

--- 2022.consoleFt/lib/main.dart
+++ 2022.consoleFt/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:js';
 import 'package:firebase_core/firebase_core.dart' show Firebase;
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -13,8 +14,8 @@ import 'log_viewer.dart';
 final db = FirebaseFirestore.instance;
 final refApp = db.collection("tmp").doc();
 
-//const defaultDevId = "default";
-const defaultDevId = "sc";
+const defaultDevId = "default";
+//const defaultDevId = "sc";
 
 Future<void> main() async {
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
@@ -39,97 +40,142 @@ class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
 
+  PopupMenuItem<Function> menuItem1(
+          BuildContext context, DocumentReference refDev) =>
+      PopupMenuItem(
+          value: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => DetectedDevicesPage(refDev)));
+          },
+          child: const Text("Detected Devices"));
+  PopupMenuItem<Function> menuItem2(
+          BuildContext context, DocumentReference refDev) =>
+      PopupMenuItem(
+          value: () async {
+            for (final e in (await refDev.collection("discovery").get()).docs) {
+              e.reference.delete();
+            }
+          }, // "clear",
+          child: const Text("Clear detected devices"));
+  PopupMenuItem<Function> menuItem3(
+          BuildContext context, DocumentReference refDev) =>
+      PopupMenuItem(
+          value: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => LogsPage(refDev)));
+          },
+          child: const Text("Vmstat Logs"));
+
+  AppBar appBar(BuildContext context, String ag, DocumentReference refDev) =>
+      AppBar(
+        title: Text("$ag - Monitor"),
+        actions: [
+          aliveIndicator(refDev.collection("discovery")),
+          PopupMenuButton<Function>(
+            initialValue: () {},
+            onSelected: (Function f) => f(),
+            itemBuilder: (BuildContext context) {
+              return [
+                menuItem1(context, refDev),
+                menuItem2(context, refDev),
+                menuItem3(context, refDev),
+              ];
+            },
+          ),
+        ],
+      );
+
   @override
   Widget build(BuildContext context) {
     return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-        stream: refApp.snapshots(),
-        builder: (context, snapshot) {
-          final ag = snapshot.data?.data()?["ag"] as String? ?? defaultDevId;
-          final refDev = db.collection("d").doc(ag);
-
-          AppBar appBar(BuildContext context, String ag) => AppBar(
-                title: Text("$ag - Scan Monitor"),
-                actions: [
-                  timeRateIndicator(refDev.collection("discovery")),
-                  PopupMenuButton<Function>(
-                    initialValue: () {},
-                    onSelected: (Function f) => f(),
-                    itemBuilder: (BuildContext context) {
-                      return [
-                        PopupMenuItem(
-                            value: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) =>
-                                        DetectedDevicesPage(refDev)),
-                              );
-                            },
-                            child: const Text("Detected Devices")),
-                        PopupMenuItem(
-                            value: () async {
-                              for (final e in (await refDev
-                                      .collection("discovery")
-                                      .get())
-                                  .docs) {
-                                e.reference.delete();
-                              }
-                            }, // "clear",
-                            child: const Text("Clear detected devices")),
-                        PopupMenuItem(
-                            value: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => LogsPage(refDev)),
-                              );
-                            },
-                            child: const Text("Vmstat Logs")),
-                      ];
-                    },
-                  ),
-                ],
-              );
-
-          Query qrDiscoveryRes = refDev
-              .collection("discovery")
-              .orderBy("time", descending: true)
-              .limit(10);
-
-          return Scaffold(
-              appBar: appBar(context, ag),
+      stream: refApp.snapshots(),
+      builder: (context, snapshot) {
+        final ag = snapshot.data?.data()?["ag"] as String? ?? defaultDevId;
+        final refDev = db.collection("d").doc(ag);
+        // AppBar appBarX(BuildContext context, String ag) =>
+        //     AppBar(title: Text("$ag - Scan Monitor"), actions: [
+        //       timeRateIndicator(refDev.collection("discovery")),
+        //       PopupMenuButton<Function>(
+        //         initialValue: () {},
+        //         onSelected: (Function f) => f(),
+        //         itemBuilder: (BuildContext context) {
+        //           return [
+        //             PopupMenuItem(
+        //                 value: () {
+        //                   Navigator.push(
+        //                     context,
+        //                     MaterialPageRoute(
+        //                         builder: (context) =>
+        //                             DetectedDevicesPage(refDev)),
+        //                   );
+        //                 },
+        //                 child: const Text("Detected Devices")),
+        //             PopupMenuItem(
+        //                 value: () async {
+        //                   for (final e
+        //                       in (await refDev.collection("discovery").get())
+        //                           .docs) {
+        //                     e.reference.delete();
+        //                   }
+        //                 }, // "clear",
+        //                 child: const Text("Clear detected devices")),
+        //             PopupMenuItem(
+        //                 value: () {
+        //                   Navigator.push(
+        //                     context,
+        //                     MaterialPageRoute(
+        //                         builder: (context) => LogsPage(refDev)),
+        //                   );
+        //                 },
+        //                 child: const Text("Vmstat Logs")),
+        //           ];
+        //         },
+        //       ),
+        //     ]);
+        return Scaffold(
+            appBar: appBar(context, ag, refDev),
 //            floatingActionButton: FloatingActionButton(
 //                child: const Icon(Icons.search), onPressed: () => {}),
-              body: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(children: [
-                  agentNameField(refApp),
-                  discSettingField(refDev),
-                  Expanded(child: RealtimeMericsWidget(refDev)),
-                  //Expanded(child: DetectedDevicesWidget(refDev)),
-                ]),
-              ));
-        });
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(children: [
+                agentNameField(refApp),
+                discSettingField(refDev),
+                Expanded(child: RealtimeMericsWidget(refDev)),
+                //Expanded(child: DetectedDevicesWidget(refDev)),
+              ]),
+            ));
+      },
+    );
   }
 }
 
-Widget timeRateIndicator(Query refLogs) {
+Widget aliveIndicator(Query refLogs) {
   Timer.periodic(const Duration(seconds: 1), (Timer t) => {});
 
-  const nLog = 10;
-  refLogs.orderBy("time", descending: true).limit(nLog);
+  const nLog = 2;
+  final now = getServerTime();
+  refLogs
+      //.where("time", isGreaterThan: now.subtract(const Duration(minutes: 3)))
+      .limit(nLog);
+  Widget alive() => const Center(
+      child: Text('Alive',
+          style: TextStyle(color: Color.fromARGB(255, 136, 179, 253))));
+  Widget noSignal() => const Center(
+      child: Text('No Signal',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Color.fromARGB(255, 238, 153, 128))));
   return StreamBuilder<QuerySnapshot>(
       stream: refLogs.snapshots(),
       builder: (_, snapshots) {
         final docsLog = snapshots.data?.docs;
         if (docsLog == null) return loadingIcon();
-        if (docsLog.length < nLog) return noItem();
-        final tNow = Timestamp.now();
+        if (docsLog.length < nLog) return noSignal();
 
-        final t0 = DiscoveryRes(docsLog.last.data()).time;
-        final td = tNow.millisecondsSinceEpoch - t0.millisecondsSinceEpoch;
-        final lps = docsLog.length * 1000 / td;
-        return Text('${lps} rps');
+        return alive();
       });
 }
 
--- 2022.consoleFt/lib/realtime_chart.dart
+++ 2022.consoleFt/lib/realtime_chart.dart
@@ -1,7 +1,6 @@
 import 'package:charts_flutter/flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:collection/collection.dart';
-import 'package:console_ft/clock.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 
@@ -9,155 +8,159 @@ import 'main.dart';
 import 'types.dart';
 
 class RealtimeMericsWidget extends StatelessWidget {
-  RealtimeMericsWidget(this.refDev, {Key? key}) : super(key: key);
+  const RealtimeMericsWidget(this.refDev, {Key? key}) : super(key: key);
   final DocumentReference refDev;
 
   @override
   Widget build(BuildContext context) {
-    final DateTime end = DateTime.now();
-    final DateTime start = DateTime.fromMillisecondsSinceEpoch(
-        end.millisecondsSinceEpoch - 60 * 60 * 1000);
-    print('start: ${start}');
-    print('end: ${end}');
-
-    final refLogs = refDev
+    const dataRange = 1 * 60 * 60 * 1000; //ms
+    var refLogs = refDev
         .collection("reports")
         .orderBy("time", descending: true)
         .where("time",
             isGreaterThanOrEqualTo: Timestamp.fromMillisecondsSinceEpoch(
-                DateTime.now().millisecondsSinceEpoch - 2 * 60 * 60 * 1000))
+                DateTime.now().millisecondsSinceEpoch - dataRange))
         .limit(120);
-
-    final domainAxis = charts.DateTimeAxisSpec(
-        viewport: DateTimeExtents(
-          start: start,
-          end: end,
-        ),
-        tickFormatterSpec: BasicDateTimeTickFormatterSpec(
-          (t) => '${t.hour}:${t.minute.toString().padLeft(2, "0")}',
-        ));
-
-    List<VmLog> vVmlog(Iterable<Log> vlog) => vlog
-        .expand((log) => log.vmlogs.toList().sorted((a, b) =>
-            b.time.millisecondsSinceEpoch - a.time.millisecondsSinceEpoch))
-        .toList();
-    List<SnmpLog> vSnmplog(Iterable<Log> vlog) => vlog
-        .expand((log) => log.snmpLogs.toList().sorted((a, b) =>
-            b.time.millisecondsSinceEpoch - a.time.millisecondsSinceEpoch))
-        .toList();
-
-    chartSeriesVmstat(
-            List<VmLog> logs, String id, int Function(VmLog) getValue) =>
-        charts.Series<VmLog, DateTime>(
-          id: id,
-          //colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
-          domainFn: (e, _) => e.time.toDate(),
-          measureFn: (e, _) => getValue(e),
-          data: logs,
-        );
-    final layout = LayoutConfig(
-      leftMarginSpec: MarginSpec.fromPixel(minPixel: 65),
-      rightMarginSpec: MarginSpec.fromPixel(minPixel: 25),
-      topMarginSpec: MarginSpec.fromPixel(minPixel: 0),
-      bottomMarginSpec: MarginSpec.fromPixel(minPixel: 35),
-    );
-    final n = getServerTime();
-
-    final List<ChartBehavior<DateTime>> commonBehaviors = [
-      charts.SeriesLegend()
-//    charts.SeriesLegend(position: BehaviorPosition.bottom)
-    ];
-
-    chartVmstatCpu(List<VmLog> vmlogs) => charts.TimeSeriesChart(
-          [
-            chartSeriesVmstat(vmlogs, "user", (v) => v.cpuUser),
-            chartSeriesVmstat(vmlogs, "sys", (v) => v.cpuSys),
-            chartSeriesVmstat(vmlogs, "wait", (v) => v.cpuWait),
-            chartSeriesVmstat(vmlogs, "stolen", (v) => v.cpuStolen),
-            chartSeriesVmstat(vmlogs, "idle", (v) => v.cpuIdle),
-          ],
-          domainAxis: domainAxis,
-          layoutConfig: layout,
-          animate: false,
-          behaviors: commonBehaviors,
-        );
-    chartVmstatQueue(List<VmLog> vmlogs) => charts.TimeSeriesChart(
-          [
-            chartSeriesVmstat(vmlogs, "proc", (v) => v.procWaitRun),
-            chartSeriesVmstat(vmlogs, "io", (v) => v.procIoBlocked),
-          ],
-          domainAxis: domainAxis,
-          layoutConfig: layout,
-          animate: false,
-          behaviors: commonBehaviors,
-        );
-    chartVmstatMemory(List<VmLog> vmlogs) => charts.TimeSeriesChart(
-          [
-            chartSeriesVmstat(vmlogs, "swap", (v) => v.memSwap),
-            chartSeriesVmstat(vmlogs, "buff", (v) => v.memBuff),
-            chartSeriesVmstat(vmlogs, "cache", (v) => v.memCache),
-            chartSeriesVmstat(vmlogs, "free", (v) => v.memFree),
-          ],
-          domainAxis: domainAxis,
-          layoutConfig: layout,
-          animate: false,
-          behaviors: commonBehaviors,
-          defaultRenderer: LineRendererConfig(includeArea: true, stacked: true),
-        );
-    chartVmstatSwapIo(List<VmLog> vmlogs) => charts.TimeSeriesChart(
-          [
-            chartSeriesVmstat(vmlogs, "sw-in", (v) => v.swapIn),
-            chartSeriesVmstat(vmlogs, "sw-out", (v) => v.swapOut),
-            chartSeriesVmstat(vmlogs, "io-in", (v) => v.ioIn),
-            chartSeriesVmstat(vmlogs, "io-out", (v) => v.ioOut),
-          ],
-          domainAxis: domainAxis,
-          layoutConfig: layout,
-          animate: false,
-          behaviors: commonBehaviors,
-        );
-    chartSnmpScan(List<SnmpLog> snmpLogs) => charts.TimeSeriesChart(
-          [
-            charts.Series<SnmpLog, DateTime>(
-              id: "scan /s",
-              domainFn: (e, _) => e.time.toDate(),
-              measureFn: (_, i) {
-                if (i == snmpLogs.length - 1) return null;
-                return (snmpLogs[i as int].scanCount -
-                        snmpLogs[i + 1].scanCount) /
-                    60;
-              },
-              data: snmpLogs,
-            ),
-          ],
-          domainAxis: domainAxis,
-          layoutConfig: layout,
-          animate: false,
-          behaviors: commonBehaviors,
-        );
-    chartSnmpDetect(List<SnmpLog> snmpLogs) => charts.TimeSeriesChart(
-          [
-            charts.Series<SnmpLog, DateTime>(
-              id: "detected /60s",
-              domainFn: (e, _) => e.time.toDate(),
-              measureFn: (_, i) {
-                if (i == snmpLogs.length - 1) return null;
-                return (snmpLogs[i as int].detectCount -
-                        snmpLogs[i + 1].detectCount) /
-                    1;
-              },
-              data: snmpLogs,
-            ),
-          ],
-          domainAxis: domainAxis,
-          layoutConfig: layout,
-          animate: false,
-          behaviors: commonBehaviors,
-        );
 
     return StreamBuilder<QuerySnapshot>(
         stream: refLogs.snapshots(),
         builder: (context, ssLogs) {
+          final DateTime end = DateTime.now();
+          final DateTime start = DateTime.fromMillisecondsSinceEpoch(
+              end.millisecondsSinceEpoch - dataRange);
+          refLogs = refLogs.where("time", isGreaterThanOrEqualTo: start);
+          final domainAxis = charts.DateTimeAxisSpec(
+              viewport: DateTimeExtents(
+                start: start,
+                end: end,
+              ),
+              tickFormatterSpec: BasicDateTimeTickFormatterSpec(
+                (t) => '${t.hour}:${t.minute.toString().padLeft(2, "0")}',
+              ));
+
+          List<VmLog> vVmlog(Iterable<Log> vlog) => vlog
+              .expand((log) => log.vmlogs.toList().sorted((a, b) =>
+                  b.time.millisecondsSinceEpoch -
+                  a.time.millisecondsSinceEpoch))
+              .toList();
+          List<SnmpLog> vSnmplog(Iterable<Log> vlog) => vlog
+              .expand((log) => log.snmpLogs.toList().sorted((a, b) =>
+                  b.time.millisecondsSinceEpoch -
+                  a.time.millisecondsSinceEpoch))
+              .toList();
+
+          chartSeriesVmstat(
+                  List<VmLog> logs, String id, int Function(VmLog) getValue) =>
+              charts.Series<VmLog, DateTime>(
+                id: id,
+                //colorFn: (_, __) => charts.MaterialPalette.blue.shadeDefault,
+                domainFn: (e, _) => e.time.toDate(),
+                measureFn: (e, _) => getValue(e),
+                data: logs,
+              );
+          final layout = LayoutConfig(
+            leftMarginSpec: MarginSpec.fromPixel(minPixel: 65),
+            rightMarginSpec: MarginSpec.fromPixel(minPixel: 25),
+            topMarginSpec: MarginSpec.fromPixel(minPixel: 0),
+            bottomMarginSpec: MarginSpec.fromPixel(minPixel: 35),
+          );
+
+          final List<ChartBehavior<DateTime>> commonBehaviors = [
+            charts.SeriesLegend(position: BehaviorPosition.top)
+            // charts.SeriesLegend(position: BehaviorPosition.inside)
+            //charts.SeriesLegend(position: BehaviorPosition.bottom)
+          ];
+
+          chartVmstatCpu(List<VmLog> vmlogs) => charts.TimeSeriesChart(
+                [
+                  chartSeriesVmstat(vmlogs, "user", (v) => v.cpuUser),
+                  chartSeriesVmstat(vmlogs, "sys", (v) => v.cpuSys),
+                  chartSeriesVmstat(vmlogs, "wait", (v) => v.cpuWait),
+                  chartSeriesVmstat(vmlogs, "stolen", (v) => v.cpuStolen),
+                  chartSeriesVmstat(vmlogs, "idle", (v) => v.cpuIdle),
+                ],
+                domainAxis: domainAxis,
+                layoutConfig: layout,
+                animate: false,
+                behaviors: commonBehaviors,
+                defaultRenderer:
+                    LineRendererConfig(includeArea: true, stacked: true),
+              );
+          chartVmstatQueue(List<VmLog> vmlogs) => charts.TimeSeriesChart(
+                [
+                  chartSeriesVmstat(vmlogs, "proc", (v) => v.procWaitRun),
+                  chartSeriesVmstat(vmlogs, "io", (v) => v.procIoBlocked),
+                ],
+                domainAxis: domainAxis,
+                layoutConfig: layout,
+                animate: false,
+                behaviors: commonBehaviors,
+              );
+          chartVmstatMemory(List<VmLog> vmlogs) => charts.TimeSeriesChart(
+                [
+                  chartSeriesVmstat(vmlogs, "swap", (v) => v.memSwap),
+                  chartSeriesVmstat(vmlogs, "buff", (v) => v.memBuff),
+                  chartSeriesVmstat(vmlogs, "cache", (v) => v.memCache),
+                  chartSeriesVmstat(vmlogs, "free", (v) => v.memFree),
+                ],
+                domainAxis: domainAxis,
+                layoutConfig: layout,
+                animate: false,
+                behaviors: commonBehaviors,
+                defaultRenderer:
+                    LineRendererConfig(includeArea: true, stacked: true),
+              );
+          chartVmstatSwapIo(List<VmLog> vmlogs) => charts.TimeSeriesChart(
+                [
+                  chartSeriesVmstat(vmlogs, "sw-in", (v) => v.swapIn),
+                  chartSeriesVmstat(vmlogs, "sw-out", (v) => v.swapOut),
+                  chartSeriesVmstat(vmlogs, "io-in", (v) => v.ioIn),
+                  chartSeriesVmstat(vmlogs, "io-out", (v) => v.ioOut),
+                ],
+                domainAxis: domainAxis,
+                layoutConfig: layout,
+                animate: false,
+                behaviors: commonBehaviors,
+              );
+          chartSnmpScan(List<SnmpLog> snmpLogs) => charts.TimeSeriesChart(
+                [
+                  charts.Series<SnmpLog, DateTime>(
+                    id: "scan /s",
+                    domainFn: (e, _) => e.time.toDate(),
+                    measureFn: (_, i) {
+                      if (i == snmpLogs.length - 1) return null;
+                      return (snmpLogs[i as int].scanCount -
+                              snmpLogs[i + 1].scanCount) /
+                          60;
+                    },
+                    data: snmpLogs,
+                  ),
+                ],
+                domainAxis: domainAxis,
+                layoutConfig: layout,
+                animate: false,
+                behaviors: commonBehaviors,
+              );
+          chartSnmpDetect(List<SnmpLog> snmpLogs) => charts.TimeSeriesChart(
+                [
+                  charts.Series<SnmpLog, DateTime>(
+                    id: "detected /60s",
+                    domainFn: (e, _) => e.time.toDate(),
+                    measureFn: (_, i) {
+                      if (i == snmpLogs.length - 1) return null;
+                      return (snmpLogs[i as int].detectCount -
+                              snmpLogs[i + 1].detectCount) /
+                          1;
+                    },
+                    data: snmpLogs,
+                  ),
+                ],
+                domainAxis: domainAxis,
+                layoutConfig: layout,
+                animate: false,
+                behaviors: commonBehaviors,
+              );
+
           if (ssLogs.hasError) return noItem();
           final vLog = ssLogs.data?.docs.map((e) => Log(e));
           if (vLog == null) return loadingIcon();
