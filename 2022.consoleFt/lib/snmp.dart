3e1583b95521056029e897dabac41a232e8c63af
kyoya.p <kyoya.p@gmail.com>
2022-12-11 21:15:22 +0900

    n

--- 2022.consoleFt/lib/snmp.dart
+++ 2022.consoleFt/lib/snmp.dart
@@ -8,8 +8,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'main.dart';
 import 'types.dart';
 
+String escaped(String s) {
+  return utf8.decode(utf8.encode(s).expand((e) {
+    if (e <= 0x20 && 0x7f <= e || e == ':') {
+      return utf8.encode(":${e & 0xff << 8}${e & 0xff}");
+    } else {
+      return [e];
+    }
+  }).toList());
+}
+
 DocumentReference<Map<String, dynamic>>? refApp;
 
+// SNMP検索条件設定Widget
 Widget discSettingField(DocumentReference<Map<String, dynamic>> docRefAg) {
   return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
       stream: docRefAg.snapshots(),
@@ -51,6 +62,7 @@ Widget discSettingField(DocumentReference<Map<String, dynamic>> docRefAg) {
       });
 }
 
+// SNMP検索結果表示(リアルタイム更新)Widget
 Widget listMonitor(Query docRefResult) {
   return StreamBuilder<QuerySnapshot>(
       stream: docRefResult.snapshots(),
@@ -65,34 +77,21 @@ Widget listMonitor(Query docRefResult) {
       });
 }
 
-Object addVbToMap(Map<int, dynamic> map, List<int> oid, String v) {
-  final key = oid[0];
-  if (oid.length == 1) {
-    map[key] = v;
-  } else {
-    map[key] = addVbToMap(map[key] ?? {}, oid.sublist(1), v);
-  }
-  return map;
-}
-
-Map<int, dynamic> addVbmToMap(Map<int, dynamic> map, Map<String, String> vbm) {
-  for (final e in vbm.entries) {
-    addVbToMap(map, oidToSeq(e.key).toList(), e.value);
-  }
-  return map;
-}
-
-Iterable<int> oidToSeq(String oid) => oid.split(".").map((e) => int.parse(e));
+// SNMP検索結果表示内の
 Widget errorList(Map<String, String> vbm) {
-  final hrPrtErr = vbm[hrPrinterDetectedErrorState + ".1"];
-  if (hrPrtErr == null) return const Text("");
-  final hrPrtErrByte = utf8.encode(hrPrtErr);
-  final flags0 = hrPrtErrByte[0].toRadixString(2).padLeft(8, '0');
-  final flags1 = hrPrtErrByte[1].toRadixString(2).padLeft(8, '0');
-  final vbl = addVbmToMap({}, vbm);
-  final n = vbl.hrPrinterDetectedErrorState();
-  print(n.firstDescendantsEntry());
-  return Text("${flags0}_$flags1");
+  var status = "";
+  final vbm2 = makeVBM(vbm);
+  final hrDevSt = vbm2.firstDescendant(oidToList(hrDeviceStatus))?.value;
+  final hrPrtSt = vbm2.firstDescendant(oidToList(hrPrinterStatus))?.value;
+  status += "$hrDevSt/$hrPrtSt/";
+  final hrPrtErr = vbm2.firstDescendant(oidToList(hrPrinterDetectedErrorState));
+  if (hrPrtErr != null) {
+    status += utf8
+        .encode(hrPrtErr.value)
+        .map((e) => e.toRadixString(2).padLeft(8, "0"))
+        .join("_");
+  }
+  return Text("$status");
 }
 
 Card discResultItemMaker(DiscoveryRes e) => Card(
@@ -154,26 +153,85 @@ List<Widget> errorIcons(int hrPrinterDetectedErrorState) {
 const hrDeviceDescr = "1.3.6.1.2.1.25.3.2.1.3";
 const hrDeviceStatus = "1.3.6.1.2.1.25.3.2.1.5";
 const hrDeviceErrors = "1.3.6.1.2.1.25.3.2.1.6";
+const hrPrinterStatus = "1.3.6.1.2.1.25.3.5.1.1";
 const hrPrinterDetectedErrorState = "1.3.6.1.2.1.25.3.5.1.2";
 const sysDescr = "1.3.6.1.2.1.1.1";
 const sysObjectID = "1.3.6.1.2.1.1.2";
 const sysName = "1.3.6.1.2.1.1.5";
 const sysLocation = "1.3.6.1.2.1.1.6";
 
-extension VBLExt on dynamic {
-  dynamic mib2() => this[1][3][6][1][2][1];
-  dynamic hrPrinterDetectedErrorState() => this.mib2()[25][3][5][1][2];
+// Local VBL Library
+Object addVbToMap(Map<int, dynamic> map, List<int> oid, String v) {
+  final key = oid[0];
+  if (oid.length == 1) {
+    map[key] = v;
+  } else {
+    map[key] = addVbToMap(map[key] ?? {}, oid.sublist(1), v);
+  }
+  return map;
+}
 
-  MapEntry<int, dynamic> firstChildEntry() =>
-      SplayTreeMap<int, dynamic>.from(this as Map).entries.first;
-  dynamic firstDescendantsEntry() {
-    if (this.value is String) {
-      return this;
-    } else if (this.value is Map) {
-      return this.firstChildEntry().value.firstDescendantsEntry();
+Map<int, dynamic> addVbmToMap(Map<int, dynamic> map, Map<String, String> vbm) {
+  for (final e in vbm.entries) {
+    addVbToMap(map, oidToSeq(e.key).toList(), e.value);
+  }
+  return map;
+}
+
+Iterable<int> oidToSeq(String oid) => oid.split(".").map((e) => int.parse(e));
+List<int> oidToList(String oid) => oidToSeq(oid).toList();
+
+extension VBMExt on SplayTreeMap<List<int>, String> {
+  String? get(List<int> oid) => this[oid];
+  MapEntry<List<int>, String>? getNext(List<int> oid) {
+    final nextOid = firstKeyAfter(oid);
+    if (nextOid == null) return null;
+    final String? value = this[nextOid];
+    if (value == null) return null;
+    return MapEntry(nextOid, value);
+  }
+
+  List<int>? firstDescendantOid(List<int> oid) {
+    bool startsWith(List<int> v, List<int> vTop) {
+      if (vTop.length > v.length) return false;
+      for (int i = vTop.length - 1; i >= 0; --i) {
+        if (vTop[i] != v[i]) return false;
+      }
+      return true;
+    }
+
+    final nextOid = firstKeyAfter(oid);
+    if (nextOid == null) return null;
+    if (startsWith(nextOid, oid)) return nextOid;
+    return null;
+  }
+
+  MapEntry<List<int>, String>? firstDescendant(List<int> oid) {
+    final tgOid = firstDescendantOid(oid);
+    if (tgOid == null) return null;
+    final tgVal = this[tgOid];
+    if (tgVal == null) return null;
+    return MapEntry(tgOid, tgVal);
+  }
+}
+
+SplayTreeMap<List<int>, String> makeVBM(Map<String, String> m) {
+  int listComparetor(List<int> a, List<int> b) {
+    if (a.length > b.length) {
+      return -listComparetor(b, a);
+    } else {
+      for (int i = 0; i < a.length; ++i) {
+        if (a[i] > b[i]) return 1;
+        if (a[i] < b[i]) return -1;
+      }
+      if (a.length < b.length) return -1;
+      return 0;
     }
   }
 
-  dynamic isNode() => this is Map;
-  dynamic isLeaf() => this is String;
+  final tgMap = SplayTreeMap<List<int>, String>(listComparetor);
+  tgMap.addEntries(m.entries.map((e) {
+    return MapEntry(oidToSeq(e.key).toList(), e.value);
+  }));
+  return tgMap;
 }
